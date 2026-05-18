0d99b3682006cd76228f9d71a1c25d60b50aa52f
javierdcov <xavierdcm@gmail.com>
2026-05-18 18:20:27 -0500

    Create firebase-config.js

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+// ─────────────────────────────────────────────────────────────
+//  FORZA SCANS — Firebase Configuration
+// ─────────────────────────────────────────────────────────────
+
+export const firebaseConfig = {
+  apiKey:            "AIzaSyA5mtggr93SjIGNXs8FRgW-jv1Ls9RYmLY",
+  authDomain:        "forza-scans.firebaseapp.com",
+  projectId:         "forza-scans",
+  storageBucket:     "forza-scans.firebasestorage.app",
+  messagingSenderId: "212292380447",
+  appId:             "1:212292380447:web:d8dd6316dd52f00cd2322a"
+};
+
+export const DB_ROOT = "forza_eventos";
